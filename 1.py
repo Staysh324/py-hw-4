5c9b21313d86a1f5e43177d2fd1bdef80d196e66
Artem <proceller@yandex.ru>
2023-04-21 02:52:14 +0300

    end first ex

--- 1.py
+++ 1.py
@@ -7,29 +7,21 @@ def quick_sort(array):
     greater = [i for i in array[1:] if i > pivot]
     return quick_sort(less) + [pivot] + quick_sort(greater)
 
-# def append_list(list):
-#     numofelement = 0
-#     for i in range(len(list)):
-#         list_element = int(input(f"введите элемент {numofelement}: "))
-#         list.append(list_element)
-#         numofelement += 1
-#         return list
+def append_list(list, leng, num):
+    numofelement = 1
+    for i in range(leng):
+        list_element = int(input(f"введите {numofelement}  элемент {num} списка: "))
+        list.append(list_element)
+        numofelement += 1
+    return list
 
 
 first_len = int(input("введите размер первого множества: "))
 second_len = int(input("введите размер второго множества: "))
 listone = []
 listtwo = []
-numofelement = 1
-for i in range(first_len):
-    list_element = int(input(f"введите {numofelement} элемент первого множества: "))
-    listone.append(list_element)
-    numofelement += 1
-numofelement = 1
-for i in range(second_len):
-    list_element = int(input(f"введите {numofelement} элемент второго множества: "))
-    listtwo.append(list_element)
-    numofelement += 1
+listone = append_list(listone, first_len, 1)
+listtwo = append_list(listtwo, second_len, 2)
 listone = set(listone)
 listtwo = set(listtwo)
 sets = listone.union(listtwo)
